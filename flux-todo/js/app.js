456d67fe0509c4ef48e37675e1dd5aa0713be383
Oliver Song <osong@mit.edu>
2015-01-14 02:26:55 -0800

    Omfg boilerplate is teaching me so much

--- flux-todo/js/app.js
+++ flux-todo/js/app.js
@@ -1,8 +1,5 @@
 /** @jsx React.DOM */
 var React = require('react');
-var requireStylesheet = require('stylesheets').requireStylesheet;
-
-requireStylesheet(process.env.STATIC_ROOT + 'MyComponent.css');
 
 var MyComponent = React.createClass({
   render: function() {
