4ade81535f9511c66dbf2595d46325eee5d7aa4b
Oliver Song <osong@mit.edu>
2015-01-16 16:28:59 -0800

    iofaew

---
+++ shopping-cart/js/constants/FluxCartConstants.js
@@ -0,0 +1,10 @@
+var keyMirror = require('react/lib/keyMirror');
+
+// Define action constants
+module.exports = keyMirror({
+  CART_ADD: null,       // Adds item to cart
+  CART_REMOVE: null,    // Remove item from cart
+  CART_VISIBLE: null,   // Shows or hides the cart
+  SET_SELECTED: null,   // Selects a product option
+  RECEIVE_DATA: null    // Loads our mock data
+});
